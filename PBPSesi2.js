7896a486629ef661820c4746cd6a1b7a5af837f4
SyarifudinFajarSuryakencana <127466332+SyarifudinFajarSuryakencana@users.noreply.github.com>
2023-10-02 21:58:40 +0700

    Add files via upload

---
+++ PBPSesi2.js
@@ -0,0 +1,8 @@
+const firstName = "Syarifudin Fajar";
+const lastName = "Suryakencana";
+const age = 17;
+const isMarried = false;
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(isMarried);
